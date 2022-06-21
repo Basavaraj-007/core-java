0989b5d3f455425ca909241596c6f66f04740672
unknown <basavaraj.xworkz@gmail.com>
2022-06-21 13:40:31 +0530

    adding Arrays files

---
+++ UserDetails.java
@@ -0,0 +1,13 @@
+class UserDetails
+{
+static String names[]={"MSDhoni","Bravo","Jadeja","AmbatiRayudu"};
+static int age[]={40,41,36,35};
+public static void main(String a[])
+{
+System.out.println("First userName is "+names[0] +"Age is "+age[0]);
+System.out.println("Second userName is "+names[1]+"Age is "+age[1]);
+System.out.println("Third userName is "+names[2]+"Age is "+age[2]);
+System.out.println("Fourth userName is "+names[3]+"Age is "+age[3]);
+
+}
+}
---
+++ UserPersonDetails.java
@@ -0,0 +1,21 @@
+class UserPersonDetails
+{
+public static void main(String a[])
+{
+String userName="Basavaraj";
+String userPlace="Siruguppa";
+int userAge=22;
+float userWeightinKg=88;
+double userHeightinft=5.8;
+long userNumber=9113911415l;
+boolean isPhoneNumberCorrect=true;
+System.out.println("userName "+userName);
+System.out.println("userPlace "+userPlace);
+System.out.println("userAge "+userAge);
+System.out.println("userWeightinKg "+userWeightinKg);
+System.out.println("userHeightinft "+userHeightinft);
+System.out.println("userNumber "+userNumber);
+System.out.println("isPhoneNumberCorrect "+isPhoneNumberCorrect);
+
+}
+}

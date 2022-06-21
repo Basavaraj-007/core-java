1ae42552e6138bdbe9164342406a51969db71424
unknown <basavaraj.xworkz@gmail.com>
2022-06-21 09:35:32 +0530

    Committing AccountLogin file

---
+++ AccountLogin.java
@@ -0,0 +1,34 @@
+class AccountLogin
+{
+//static long contactNo=9113911415l;
+//static String email="rajdhoni707@gmail.com";
+//static boolean isContactNo;
+public static void main(String a[])
+{
+	//System.out.println("isContactNo :"+contactNo);
+login(785492552l);
+login("adsercrgfg@gmail.com");
+
+}
+public static void login(long contactNo)
+{
+     //if (isContactNo == false)
+	//{
+		
+	//isContactNo = true;
+System.out.println("login using contactNo :"+contactNo);
+	//}
+
+
+ //else if(isContactNo == false)
+//{
+//isContactNo = ;	
+	//System.out.println("use another way");
+}
+
+
+public static void login(String email)
+{
+System.out.println("login using email :"+email);
+} 
+}
